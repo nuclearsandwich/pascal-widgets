bde68a2acb041b5f54a0eb295a8c701a33c1c6b2
Steven! Ragnarök <steven@nuclearsandwich.com>
2012-08-27 18:07:58 -0700

    Shuffle around procedures and get employee built.

--- Widgets.pas
+++ Widgets.pas
@@ -3,8 +3,10 @@ CONST
 	nullb = #0;
 
 TYPE
-	textfield = packed array [1..64] of char;
+	textfield  = packed array [1..64] of char;
+
 	employee = RECORD
+		count       : integer;
 		dept        : integer;
 		id          : integer;
 		name        : textfield;
@@ -18,18 +20,11 @@ TYPE
 	state      = array [1..12] of plant;
 
 VAR
+	empl  : employee;
 	world : array [1..12] of state;
 
 PROCEDURE readempl;
-VAR
-	count : integer;
-	dept  : integer;
-	id    : integer;
-	name  : textfield;
-	plant : integer;
-	state : integer;
-
-	PROCEDURE readname(VAR name : textfield);
+	PROCEDURE readname;
 	VAR
 		ch : char;
 		i  : integer;
@@ -40,40 +35,45 @@ VAR
 			read(ch);
 		WHILE ch <> ':' DO
 		BEGIN
-			name[i] := ch;
+			empl.name[i] := ch;
 			i := i + 1;
 			read(ch);
 		END;
-		name[i] := nullb;
+		empl.name[i] := nullb;
 		read(ch);
 	END;
 
-	PROCEDURE writename(name : textfield);
+BEGIN
+	read(empl.state);
+	read(empl.plant);
+	read(empl.dept);
+	read(empl.id);
+	readname;
+	read(empl.count);
+END;
+
+PROCEDURE writeempl;
+	PROCEDURE writename;
 	VAR
 		i : integer;
 	BEGIN
 		i := 1;
-		WHILE name[i] <> nullb DO
+		WHILE empl.name[i] <> nullb DO
 		BEGIN
-			write(name[i]);
+		write(ErrOutput, empl.name[i]);
 			i := i + 1;
 		END;
 	END;
-
 BEGIN
-	read(state);
-	read(plant);
-	read(dept);
-	read(id);
-	readname(name);
-	read(count);
-	write('State: ', state, ' Plant: ', plant, ' Dept: ', dept, ' ID: ', id, ' Name: ');
-	writename(name);
-	write(' Widgets: ', count);
-	writeln;
-	employee();
-END;
 
+	WITH empl DO
+	BEGIN
+		write(ErrOutput, 'State: ', state, ' Plant: ', plant, ' Dept: ', dept, ' ID: ', id, ' Name: ');
+		writename;
+		write(ErrOutput, ' Widgets: ', count);
+		writeln(ErrOutput);
+	END
+END;
 
 BEGIN
 	readempl;
